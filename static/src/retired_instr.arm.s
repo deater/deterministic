93c22a4c3054be948f1d8f048ce6274861f7c443
Vince Weaver <vincent.weaver@maine.edu>
2014-06-17 23:09:51 +0200

    Add some ARM logic instructions

--- static/src/retired_instr.arm.s
+++ static/src/retired_instr.arm.s
@@ -4,19 +4,22 @@
 	# by Vince Weaver, vincent.weaver at maine.edu
 	# 17 June 2014
 
-	#####################################################
-	# Retired Instructions
-	#####################################################
+	###############################################################
+	# Retired Instructions		expected		actual
+	###############################################################
+	# arithmetic		=	202,001			202,001
         # OS			=	?
-	# -----------------------------------
-	# Expected		=	?
+	# General Overhead	=	     16
+	# --------------------------------------------------------------
+	# Expected		=	202,017			202,016
 
 	##################################################################
 	# Retired Branches		expected		actual
 	##################################################################
-	# General Overhead 	=
-	# -----------------------------------
-	# Expected		=
+	# Arithmeitc		=	1,000			1,000
+	# General Overhead 	=	    1			    1
+	# ---------------------------------------------------------------
+	# Expected		=	1,001			1,001
 
 
 	##################################################################
@@ -302,15 +305,254 @@ test_arith:
 	# Logic and Bit Ops
 	######################################################
 
-	# ssat usat 
+	# and(s) bic(s) eor(s) orr(s) orn(s)
+	# bfc, bfi
+	# rbit rev rev16 revsh sbfx sxtb sxth ubfx uxtb uxth
+	# cmp cmn teq tst
+	# asr(s) lsl(s) lsr(s) ror(s) rrx(s)
+
+
+
+
+.if LOGIC==1
+
+	mov	r0,#1000
+
+test_logic:
+
+	and	r1,r1,#1		@ i1
+	and	r1,r2,r3,LSL #8		@ i2
+	and	r2,r3,r4,LSR #2		@ i3
+	and	r3,r4,r5,ASR #3		@ i4
+	and	r6,r7,r8,ROR #5		@ i5
+	and	r7,r8,r9,RRX		@ i6
+	andeq	r9,r10,r10		@ i7
+	andne	r9,r10,r10		@ i8
+	andcs	r9,r10,r10		@ i9
+	andcc	r9,r10,r10		@ i10
+	andmi	r9,r10,r10		@ i11
+	andpl	r9,r10,r10		@ i12
+	andhi	r9,r10,r10		@ i13
+	andls	r9,r10,r10		@ i14
+	andge	r9,r10,r10		@ i15
+	andgt	r9,r10,r10		@ i16
+	andle	r9,r10,r10		@ i17
+	andlt	r9,r10,r10		@ i18
+	andal	r9,r10,r10		@ i19
+	and	r1,r1,pc		@ i20
+
+	ands	r1,r1,#1		@ i21
+	ands	r1,r2,r3,LSL #8		@ i22
+	ands	r2,r3,r4,LSR #2		@ i23
+	ands	r3,r4,r5,ASR #3		@ i24
+	ands	r6,r7,r8,ROR #5		@ i25
+	ands	r7,r8,r9,RRX		@ i26
+	andeqs	r9,r10,r10		@ i27
+	andnes	r9,r10,r10		@ i28
+	andcss	r9,r10,r10		@ i29
+	andccs	r9,r10,r10		@ i30
+	andmis	r9,r10,r10		@ i31
+	andpls	r9,r10,r10		@ i32
+	andhis	r9,r10,r10		@ i33
+	andlss	r9,r10,r10		@ i34
+	andges	r9,r10,r10		@ i35
+	andgts	r9,r10,r10		@ i36
+	andles	r9,r10,r10		@ i37
+	andlts	r9,r10,r10		@ i38
+	andals	r9,r10,r10		@ i39
+	ands	r1,r1,pc		@ i40
+
+	bic	r1,r1,#1		@ i41
+	bic	r1,r2,r3,LSL #8		@ i42
+	bic	r2,r3,r4,LSR #2		@ i43
+	bic	r3,r4,r5,ASR #3		@ i44
+	bic	r6,r7,r8,ROR #5		@ i45
+	bic	r7,r8,r9,RRX		@ i46
+	biceq	r9,r10,r10		@ i47
+	bicne	r9,r10,r10		@ i48
+	biccs	r9,r10,r10		@ i49
+	biccc	r9,r10,r10		@ i50
+	bicmi	r9,r10,r10		@ i51
+	bicpl	r9,r10,r10		@ i52
+	bichi	r9,r10,r10		@ i53
+	bicls	r9,r10,r10		@ i54
+	bicge	r9,r10,r10		@ i55
+	bicgt	r9,r10,r10		@ i56
+	bicle	r9,r10,r10		@ i57
+	biclt	r9,r10,r10		@ i58
+	bical	r9,r10,r10		@ i59
+	bic	r1,r1,pc		@ i60
+
+	bics	r1,r1,#1		@ i61
+	bics	r1,r2,r3,LSL #8		@ i62
+	bics	r2,r3,r4,LSR #2		@ i63
+	bics	r3,r4,r5,ASR #3		@ i64
+	bics	r6,r7,r8,ROR #5		@ i65
+	bics	r7,r8,r9,RRX		@ i66
+	biceqs	r9,r10,r10		@ i67
+	bicnes	r9,r10,r10		@ i68
+	biccss	r9,r10,r10		@ i69
+	bicccs	r9,r10,r10		@ i70
+	bicmis	r9,r10,r10		@ i71
+	bicpls	r9,r10,r10		@ i72
+	bichis	r9,r10,r10		@ i73
+	biclss	r9,r10,r10		@ i74
+	bicges	r9,r10,r10		@ i75
+	bicgts	r9,r10,r10		@ i76
+	bicles	r9,r10,r10		@ i77
+	biclts	r9,r10,r10		@ i78
+	bicals	r9,r10,r10		@ i79
+	bics	r1,r1,pc		@ i80
+
+	eor	r1,r1,#1		@ i81
+	eor	r1,r2,r3,LSL #8		@ i82
+	eor	r2,r3,r4,LSR #2		@ i83
+	eor	r3,r4,r5,ASR #3		@ i84
+	eor	r6,r7,r8,ROR #5		@ i85
+	eor	r7,r8,r9,RRX		@ i86
+	eoreq	r9,r10,r10		@ i87
+	eorne	r9,r10,r10		@ i88
+	eorcs	r9,r10,r10		@ i89
+	eorcc	r9,r10,r10		@ i90
+	eormi	r9,r10,r10		@ i91
+	eorpl	r9,r10,r10		@ i92
+	eorhi	r9,r10,r10		@ i93
+	eorls	r9,r10,r10		@ i94
+	eorge	r9,r10,r10		@ i95
+	eorgt	r9,r10,r10		@ i96
+	eorle	r9,r10,r10		@ i97
+	eorlt	r9,r10,r10		@ i98
+	eoral	r9,r10,r10		@ i99
+	eor	r1,r1,pc		@ i100
+
+	eors	r1,r1,#1		@ i101
+	eors	r1,r2,r3,LSL #8		@ i102
+	eors	r2,r3,r4,LSR #2		@ i103
+	eors	r3,r4,r5,ASR #3		@ i104
+	eors	r6,r7,r8,ROR #5		@ i105
+	eors	r7,r8,r9,RRX		@ i106
+	eoreqs	r9,r10,r10		@ i107
+	eornes	r9,r10,r10		@ i108
+	eorcss	r9,r10,r10		@ i109
+	eorccs	r9,r10,r10		@ i110
+	eormis	r9,r10,r10		@ i111
+	eorpls	r9,r10,r10		@ i112
+	eorhis	r9,r10,r10		@ i113
+	eorlss	r9,r10,r10		@ i114
+	eorges	r9,r10,r10		@ i115
+	eorgts	r9,r10,r10		@ i116
+	eorles	r9,r10,r10		@ i117
+	eorlts	r9,r10,r10		@ i118
+	eorals	r9,r10,r10		@ i119
+	eors	r1,r1,pc		@ i120
+
+
+	orr	r1,r1,#1		@ i121
+	orr	r1,r2,r3,LSL #8		@ i122
+	orr	r2,r3,r4,LSR #2		@ i123
+	orr	r3,r4,r5,ASR #3		@ i124
+	orr	r6,r7,r8,ROR #5		@ i125
+	orr	r7,r8,r9,RRX		@ i126
+	orreq	r9,r10,r10		@ i127
+	orrne	r9,r10,r10		@ i128
+	orrcs	r9,r10,r10		@ i129
+	orrcc	r9,r10,r10		@ i130
+	orrmi	r9,r10,r10		@ i131
+	orrpl	r9,r10,r10		@ i132
+	orrhi	r9,r10,r10		@ i133
+	orrls	r9,r10,r10		@ i134
+	orrge	r9,r10,r10		@ i135
+	orrgt	r9,r10,r10		@ i136
+	orrle	r9,r10,r10		@ i137
+	orrlt	r9,r10,r10		@ i138
+	orral	r9,r10,r10		@ i139
+	orr	r1,r1,pc		@ i140
+
+	orrs	r1,r1,#1		@ i141
+	orrs	r1,r2,r3,LSL #8		@ i142
+	orrs	r2,r3,r4,LSR #2		@ i143
+	orrs	r3,r4,r5,ASR #3		@ i144
+	orrs	r6,r7,r8,ROR #5		@ i145
+	orrs	r7,r8,r9,RRX		@ i146
+	orreqs	r9,r10,r10		@ i147
+	orrnes	r9,r10,r10		@ i148
+	orrcss	r9,r10,r10		@ i149
+	orrccs	r9,r10,r10		@ i150
+	orrmis	r9,r10,r10		@ i151
+	orrpls	r9,r10,r10		@ i152
+	orrhis	r9,r10,r10		@ i153
+	orrlss	r9,r10,r10		@ i154
+	orrges	r9,r10,r10		@ i155
+	orrgts	r9,r10,r10		@ i156
+	orrles	r9,r10,r10		@ i157
+	orrlts	r9,r10,r10		@ i158
+	orrals	r9,r10,r10		@ i159
+	orrs	r1,r1,pc		@ i160
+
+
+	@ Thumb only
+.if 0
+	orn	r1,r1,#1		@ i161
+	orn	r1,r2,r3,LSL #8		@ i162
+	orn	r2,r3,r4,LSR #2		@ i163
+	orn	r3,r4,r5,ASR #3		@ i164
+	orn	r6,r7,r8,ROR #5		@ i165
+	orn	r7,r8,r9,RRX		@ i166
+	orneq	r9,r10,r10		@ i167
+	ornne	r9,r10,r10		@ i168
+	orncs	r9,r10,r10		@ i169
+	orncc	r9,r10,r10		@ i170
+	ornmi	r9,r10,r10		@ i171
+	ornpl	r9,r10,r10		@ i172
+	ornhi	r9,r10,r10		@ i173
+	ornls	r9,r10,r10		@ i174
+	ornge	r9,r10,r10		@ i175
+	orngt	r9,r10,r10		@ i176
+	ornle	r9,r10,r10		@ i177
+	ornlt	r9,r10,r10		@ i178
+	ornal	r9,r10,r10		@ i179
+	orn	r1,r1,pc		@ i180
+
+	orns	r1,r1,#1		@ i181
+	orns	r1,r2,r3,LSL #8		@ i182
+	orns	r2,r3,r4,LSR #2		@ i183
+	orns	r3,r4,r5,ASR #3		@ i184
+	orns	r6,r7,r8,ROR #5		@ i185
+	orns	r7,r8,r9,RRX		@ i186
+	orneqs	r9,r10,r10		@ i187
+	ornnes	r9,r10,r10		@ i188
+	orncss	r9,r10,r10		@ i189
+	ornccs	r9,r10,r10		@ i190
+	ornmis	r9,r10,r10		@ i191
+	ornpls	r9,r10,r10		@ i192
+	ornhis	r9,r10,r10		@ i193
+	ornlss	r9,r10,r10		@ i194
+	ornges	r9,r10,r10		@ i195
+	orngts	r9,r10,r10		@ i196
+	ornles	r9,r10,r10		@ i197
+	ornlts	r9,r10,r10		@ i198
+	ornals	r9,r10,r10		@ i199
+	orns	r1,r1,pc		@ i200
+.endif
+
+
+
+	subs	r0,r0,#1
+	bne	test_logic
+
+
+
+
+.endif
+
+	#################################################
+	# move/load/store/sign-extend
+	#################################################
+
+
 
 	# mov(s) mvn(s)
 
-	# and(s), bfc, bfi, bic(s), cmp, cmn, eor(s) orr(s) orn(s)
-	# rbit rev rev16 revsh sbfx sxtb sxth ubfx uxtb uxth
-	# teq tst 
-
-	# asr(s) lsl(s) lsr(s) ror(s) rrx(s)
 
 	# ldm(!) ldmdb(!) ldmea(!) ldmfd(!) ldmia(!)
 	# ldr ldrb ldrbt ldrd ldrex ldrexb ldrexh
@@ -323,24 +565,16 @@ test_arith:
 	# strt
 
 
+
 	# pop push 
 
 	# b, bl, blx, bx,
 
 	# cbz, cbnz, clz, it movt movw tbb tbh
+	# ssat usat 
 
 	# dmb,dsb,svc
 
-
-.if LOGIC==1
-
-test_logic:
-
-.endif
-
-	#################################################
-	# move/load/store/sign-extend
-	#################################################
 
 .if MOVES==1
 
